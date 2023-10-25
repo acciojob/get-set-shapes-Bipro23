a82a65f492dddd11f5d9d2237d3bf1b9c81aa7ed
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-10-25 17:42:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
 
-class Square extends Animal {}
+	constructor(width,height){this._width = width;this._height = height;}
+	
+get width(){
+	return this._width;
+}
+get height(){
+	return this._height;
+}
+
+getArea(){
+		return this._width*this._height;
+	}
+
+}
+
+
+class Square extends Rectangle {
+	constructor(side){ super(side,side); }
+
+	getPerimeter(){
+		return this.width*4;
+	}
+	
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
